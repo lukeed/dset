7bd62e756725cd7bb2c6e01c124438cbc43e53a3
Luke Edwards <luke.edwards05@gmail.com>
2018-02-20 10:19:56 -0800

    add module logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-export default function () {
-	//
+export default function (obj, keys, val) {
+	keys.split && (keys=keys.split('.'));
+	var i=0, j, o, x, len=keys.length;
+	while (i < len) {
+		o = obj;
+		for (j=0; j < i; j++) o=o[keys[j]];
+		x = o[keys[i]] || {};
+		o[keys[i]] = (++i === len) ? val : x;
+	}
 }
