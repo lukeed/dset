9fbbfc5845742a1f14aede9c325426c9891acd37
Luke Edwards <luke.edwards05@gmail.com>
2018-02-21 11:30:27 -0800

    update built files

--- dist/deepset.js
+++ dist/deepset.js
@@ -1,10 +1,8 @@
 module.exports = function (obj, keys, val) {
 	keys.split && (keys=keys.split('.'));
-	var i=0, j, o, x, len=keys.length;
-	while (i < len) {
-		o = obj;
-		for (j=0; j < i; j++) o=o[keys[j]];
-		x = (o[keys[i]] == null) ? {} : o[keys[i]];
-		o[keys[i]] = (++i === len) ? val : x;
+	var i=0, l=keys.length, t=obj, x;
+	for (; i < l; ++i) {
+		x = t[keys[i]];
+		t = t[keys[i]] = (i === l - 1 ? val : (x == null ? {} : x));
 	}
 }
