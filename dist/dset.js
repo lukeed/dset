30e9c8a831794bb3f10503e581b4ea68a17cc9ce
Luke Edwards <luke.edwards05@gmail.com>
2018-09-06 22:22:46 -0700

    remove `dist` from repo

--- dist/dset.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (obj, keys, val) {
-	keys.split && (keys=keys.split('.'));
-	var i=0, l=keys.length, t=obj, x;
-	for (; i < l; ++i) {
-		x = t[keys[i]];
-		t = t[keys[i]] = (i === l - 1 ? val : (x == null ? {} : x));
-	}
-}
