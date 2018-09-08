2b9ec49e231107b1a83b04a1bc1a66a8d14cea1c
Luke Edwards <luke.edwards05@gmail.com>
2018-09-08 13:23:24 -0700

    fix: prevent array creation w/ float-type keys
    - Closes #11

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ export default function (obj, keys, val) {
 	var i=0, l=keys.length, t=obj, x;
 	for (; i < l; ++i) {
 		x = t[keys[i]];
-		t = t[keys[i]] = (i === l - 1 ? val : (x == null ? (+keys[i + 1] > -1 ? [] : {}) : x));
+		t = t[keys[i]] = (i === l - 1 ? val : (x != null ? x : (!!~keys[i+1].indexOf('.') || !(+keys[i+1] > -1)) ? {} : []));
 	}
 }
