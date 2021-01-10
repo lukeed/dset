144f1d24ccb28bf0a9d351c935c814f047ddc6d2
Arjun Shibu <arjunshibu1999@gmail.com>
2021-01-10 14:08:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ export default function (obj, keys, val) {
 	keys.split && (keys=keys.split('.'));
 	var i=0, l=keys.length, t=obj, x;
 	for (; i < l; ++i) {
+		if (isPrototypePolluted(keys[i])) continue;
 		x = t[keys[i]];
 		t = t[keys[i]] = (i === l - 1 ? val : (x != null ? x : (!!~keys[i+1].indexOf('.') || !(+keys[i+1] > -1)) ? {} : []));
 	}
+}
+
+function isPrototypePolluted(key) {
+    return ['__proto__', 'constructor', 'prototype'].includes(key);
 }
