15338436dd05256f9f12cbc0d54d874276d81d17
Luke Edwards <luke.edwards05@gmail.com>
2021-02-24 09:19:28 -0800

    chore: add more `objects` tests

--- test/suites/objects.js
+++ test/suites/objects.js
@@ -7,7 +7,28 @@ export default function (dset, isMerge) {
 	const objects = suite('objects');
 	const verb = isMerge ? 'merge' : 'overwrite';
 
-	objects(`should ${verb} existing object value`, () => {
+	objects(`should ${verb} existing object value :: simple`, () => {
+		let { input } = prepare({
+			hello: { a: 1 }
+		});
+
+		dset(input, 'hello', { foo: 123 });
+
+		if (isMerge) {
+			assert.equal(input, {
+				hello: {
+					a: 1,
+					foo: 123,
+				}
+			});
+		} else {
+			assert.equal(input, {
+				hello: { foo: 123 }
+			});
+		}
+	});
+
+	objects(`should ${verb} existing object value :: nested`, () => {
 		let { input, copy } = prepare({
 			a: {
 				b: {
@@ -25,6 +46,49 @@ export default function (dset, isMerge) {
 		}
 
 		assert.equal(input, copy);
+	});
+
+	objects(`should ${verb} existing array value :: simple`, () => {
+		let { input } = prepare([
+			{ foo: 1 },
+		]);
+
+		dset(input, '0', { bar: 2 });
+
+		if (isMerge) {
+			assert.equal(input, [
+				{ foo: 1, bar: 2 }
+			]);
+		} else {
+			assert.equal(input, [
+				{ bar: 2 }
+			]);
+		}
+	});
+
+	objects(`should ${verb} existing array value :: nested`, () => {
+		let { input } = prepare([
+			{ name: 'bob', age: 56, friends: ['foobar'] },
+			{ name: 'alice', age: 47, friends: ['mary'] },
+		]);
+
+		dset(input, '0', { age: 57, friends: ['alice', 'mary'] });
+		dset(input, '1', { friends: ['bob'] });
+		dset(input, '2', { name: 'mary', age: 49, friends: ['bob'] });
+
+		if (isMerge) {
+			assert.equal(input, [
+				{ name: 'bob', age: 57, friends: ['alice', 'mary'] },
+				{ name: 'alice', age: 47, friends: ['bob'] },
+				{ name: 'mary', age: 49, friends: ['bob'] },
+			]);
+		} else {
+			assert.equal(input, [
+				{ age: 57, friends: ['alice', 'mary'] },
+				{ friends: ['bob'] },
+				{ name: 'mary', age: 49, friends: ['bob'] },
+			]);
+		}
 	});
 
 	objects.run();
