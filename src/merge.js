c29c251d1f8c5291109a711484cc92f669ff35a3
Luke Edwards <luke.edwards05@gmail.com>
2021-10-26 17:54:49 -0700

    chore: update module sizes;
    - related #29

--- src/merge.js
+++ src/merge.js
@@ -17,7 +17,7 @@ export function merge(a, b, k) {
 export function dset(obj, keys, val) {
 	keys.split && (keys=keys.split('.'));
 	var i=0, l=keys.length, t=obj, x, k;
-	for (; i < l;) {
+	while (i < l) {
 		k = keys[i++];
 		if (k === '__proto__' || k === 'constructor' || k === 'prototype') break;
 		t = t[k] = (i === l) ? merge(t[k],val) : (typeof(x=t[k])===typeof keys) ? x : (keys[i]*0 !== 0 || !!~(''+keys[i]).indexOf('.')) ? {} : [];
