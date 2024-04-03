931b0ed4d1c872814c4e21d247bb262175a06738
Dair Aidarkhanov <36963426+aidarkhanov@users.noreply.github.com>
2024-04-03 04:02:02 +0300

    Add typing for `merge`
    Includes type definitions to handle deep merging of nested objects and overwriting arrays.
    
    The types ensure that primitive values from the second argument (U) overwrite those in the first (T), while arrays are replaced entirely, not merged element-wise.
    
    Example:
    
    ```typescript
    const obj1 = { a: { b: [1, 2] }, c: null, d: "jaja" };
    const obj2 = { a: { b: [3, 4], c: [5, 6] } };
    
    const result = merge(obj1, obj2);
    // const result: {
    //   a: {
    //     c: number[];
    //     b: number[];
    //   };
    //   c: null;
    //   d: string;
    // }
    ```

--- src/merge.d.ts
+++ src/merge.d.ts
@@ -1,2 +1,11 @@
-export function merge(foo: any, bar: any): any; // TODO
+type Primitive = string | number | boolean | bigint | symbol | undefined | null;
+type DeepMerge<T, U> =
+	[T, U] extends [Primitive, Primitive] ? U :
+	T extends Primitive ? U :
+	U extends Primitive ? U :
+	T extends Array<unknown> ? U :
+	U extends Array<unknown> ? U :
+	{ [K in keyof T | keyof U]: K extends keyof U ? DeepMerge<T[K], U[K]> : K extends keyof T ? T[K] : never };
+
+export function merge<T extends object, U extends object>(a: T, b: U): DeepMerge<T, U>;
 export function dset<T extends object, V>(obj: T, keys: string | ArrayLike<string | number>, value: V): void;
